8ed729ee5ed8349a036b9f04a38ad612aeddbac5
Shaker Yousef <shakrshrfa@gmail.com>
2023-11-04 23:24:28 +0200

    fix error

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect,useState } from 'react';
-import ProductCard from './productCard';
+import ProductCard from './ProductCard';
 
 export default function Products() {
   let [products,setProducts] = useState([]);
